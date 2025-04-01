c52d14ed60a0592c86e59053295dc1690c6f1fcb
Angelina Ju <angelinaju2015@gmail.com>
2025-03-31 23:37:43 -0400

    ros2 tutorial & milestones

---
+++ ros2tutorials/subscriber.py
@@ -0,0 +1,31 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class MinimalSubscriber(Node):
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'Received: {msg.data}')
+
+def main(args=None):
+    rclpy.init(args=args)
+    subscriber = MinimalSubscriber()
+    
+    try:
+        rclpy.spin(subscriber)
+    except KeyboardInterrupt:
+        pass
+    
+    subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
